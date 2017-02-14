f557e2846dbcb354935c1889cb04158410d042e3
Work Ehm <ehm@hm.edu>
2017-02-14 11:27:12 -0800

    Initial texture loading

---
+++ src/Texture.cpp
@@ -0,0 +1,59 @@
+#include "Texture.h"
+#include "GLSL.h"
+
+Texture::Texture() {}
+
+Texture::~Texture() {}
+
+void Texture::loadTexture(std::string path, std::string newName) {
+
+    name = newName;
+
+    unsigned char* textureData = stbi_load( path.c_str() , &width, &height, &components, STBI_rgb_alpha );
+
+    glActiveTexture(GL_TEXTURE0);
+    glGenTextures(1, &tid);
+    glBindTexture(GL_TEXTURE_2D, tid);
+
+    if(components == 3) {
+        // RGB texture
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLubyte*) textureData);
+    } else if (components == 4) {
+        // RBGA texture
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, (GLubyte*) textureData);
+    }
+
+    // generate MipMap and set minification ang magnification filter
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // texture data not needed anymore
+    stbi_image_free(textureData);
+
+}
+
+void Texture::bind(int newUnit)
+{
+    unit = newUnit;
+    glActiveTexture(GL_TEXTURE0 + unit);
+    glBindTexture(GL_TEXTURE_2D, tid);
+    glUniform1i(0, unit);
+}
+
+void Texture::unbind() {
+    glActiveTexture(GL_TEXTURE0 + unit);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture::setHandle(GLint h) {
+    handle = h;
+}
+
+GLint Texture::getHandle() {
+    return handle;
+}
---
+++ src/Texture.h
@@ -0,0 +1,41 @@
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+//#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <string>
+
+
+class Texture
+{
+public:
+    Texture();
+    virtual ~Texture();
+
+    std::string name;
+
+    // loads the texture under the specified path. channels = channels per pixel, 3 for RGB, 4 for RGBA
+    void loadTexture(std::string path, std::string newName);
+    void bind(int newUnit);
+    void unbind();
+    void setHandle(GLint h);
+    GLint getHandle();
+
+
+private:
+
+    int width;
+    int height;
+    int components;
+
+    GLuint tid;
+    int unit;
+    GLint handle;
+
+};
+
+
+#endif

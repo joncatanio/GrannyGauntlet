fca26a9d5a06fd63b533b51e7c6601a38d0a2fa8
rgarmsen2295 <rgarmsen2295@gmail.com>
2017-02-20 19:45:53 -0800

    First pass of sliding collision (does not work with multiple colliding objs, e.g. corners)

--- src/PlayerPhysicsComponent.cpp
+++ src/PlayerPhysicsComponent.cpp
@@ -13,7 +13,50 @@ void PlayerPhysicsComponent::initObjectPhysics() {
 }
 
 void PlayerPhysicsComponent::updatePhysics(float deltaTime) {
-   if (holder_->toggleMovement) {
+
+   if (holder_->toggleMovement && holder_->velocity != 0.0f) {
+      glm::vec3 oldPosition = holder_->getPosition();
+      glm::vec3 newPosition = oldPosition + (holder_->velocity *
+         glm::normalize(holder_->direction) * deltaTime);
+      holder_->setPosition(newPosition);
+      updateBoundingBox();
+
+      // Check player collision against static objects
+      GameWorld& world = GameManager::instance().getGameWorld();
+      std::shared_ptr<GameObject> objHit = world.checkCollision(holder_);
+      if (objHit != nullptr) {
+         GameObjectType objTypeHit = objHit->type;
+
+         if (objTypeHit == GameObjectType::STATIC_OBJECT) {
+            BoundingBox* objHitBB = objHit->getBoundingBox();
+            glm::vec3 normalOfObjHit = objHitBB->calcReflNormal(getBoundingBox());
+
+            if (normalOfObjHit.x != 0.0f) {
+               newPosition.x = oldPosition.x;
+            }
+
+            if (normalOfObjHit.z != 0.0f) {
+               newPosition.z = oldPosition.z;
+            }
+
+            holder_->setPosition(newPosition);
+            updateBoundingBox();
+         }
+         if (objTypeHit == GameObjectType::FINISH_OBJECT) {
+            GameManager::instance().gameOver_ = true;
+         }
+      }
+   }
+
+
+
+
+
+
+
+
+
+   /*if (holder_->toggleMovement) {
       glm::vec3 newPosition = holder_->getPosition() + (holder_->velocity *
          glm::normalize(holder_->direction) * deltaTime);
       holder_->setPosition(newPosition);
@@ -37,5 +80,5 @@ void PlayerPhysicsComponent::updatePhysics(float deltaTime) {
       if (objTypeHit == GameObjectType::FINISH_OBJECT) {
          GameManager::instance().gameOver_ = true;
       }
-   }
+   }*/
 }

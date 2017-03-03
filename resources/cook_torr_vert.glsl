39f6b3b3976ddbd2847fa5dc58d1bf73bcbe70d4
rgarmsen2295 <rgarmsen2295@gmail.com>
2017-03-02 23:48:09 -0800

    Add cook torr shader fiels

---
+++ resources/cook_torr_vert.glsl
@@ -0,0 +1,26 @@
+#version 330 core
+
+layout(location = 0) in vec4 vertPos;
+layout(location = 1) in vec3 vertNor;
+
+uniform mat4 P;
+uniform mat4 M;
+uniform mat4 V;
+uniform mat4 tiM;
+
+// Push the position in camera space and normal in world space
+// to the fragment shader
+out vec3 positionInCamSpace;
+out vec3 normalInWorldSpace;
+
+void main() {
+
+	// Set the position of the vertex in homogeneous space
+	gl_Position = P * V * M * vertPos;
+
+	// Calculate the position of the vertex in camera/view space
+	positionInCamSpace = (V * M * vertPos).xyz;
+
+	// Calculate the normal of the vertex in world space
+	normalInWorldSpace = normalize((tiM * vec4(normalize(vertNor), 0.0)).xyz);
+}

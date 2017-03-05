19d1d949c8fcaf5669f99bd53a5d4849e947eeea
Reed Garmsen <rgarmsen2295@gmail.com>
2017-03-05 15:29:13 -0800

    Add billboard render comp

---
+++ src/BillboardRenderComponent.h
@@ -0,0 +1,15 @@
+#ifndef BILLBOARD_RENDER_COMPONENT_H
+#define BILLBOARD_RENDER_COMPONENT_H
+
+class BillboardRenderComponent {
+public:
+
+	BillboardRenderComponent();
+
+	~BillboardRenderComponent();
+
+private:
+
+};
+
+#endif

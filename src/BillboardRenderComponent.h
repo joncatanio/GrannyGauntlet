ebb18a9dd5e6e1eb9ef2bd5ea20825aa0c9ae276
Reed Garmsen <rgarmsen2295@gmail.com>
2017-03-06 01:53:35 -0800

    Initial Billboard creation logic

--- src/BillboardRenderComponent.h
+++ src/BillboardRenderComponent.h
@@ -1,12 +1,20 @@
 #ifndef BILLBOARD_RENDER_COMPONENT_H
 #define BILLBOARD_RENDER_COMPONENT_H
 
-class BillboardRenderComponent {
+#include "ShaderManager.h"
+
+#include "RenderComponent.h"
+
+class BillboardRenderComponent : public RenderComponent {
 public:
 
-	BillboardRenderComponent();
+	BillboardRenderComponent(std::shared_ptr<Shape> shape, const std::string& shaderName, std::shared_ptr<Material> material);
 
 	~BillboardRenderComponent();
+
+	void draw(std::shared_ptr<MatrixStack> P, std::shared_ptr<MatrixStack> M, std::shared_ptr<MatrixStack> V);
+
+	void renderShadow(std::shared_ptr<MatrixStack> M);
 
 private:
 

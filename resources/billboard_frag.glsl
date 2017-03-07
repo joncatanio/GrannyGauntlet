125c2167692f83660d964464615670d5d9c0b317
Reed Garmsen <rgarmsen2295@gmail.com>
2017-03-07 00:22:52 -0800

    Temporary billboard changes

--- resources/billboard_frag.glsl
+++ resources/billboard_frag.glsl
@@ -2,11 +2,15 @@
 
 in vec3 vTexCoord;
 
+<<<<<<< 9d205b09183c2c91a24a07870b944c46a1346576
 <<<<<<< 678a7567b1118afe499b003031fbbe261ca9cc44
 uniform sampler2D billboardTex;
 =======
 uniform samplerCube cubemap;
 >>>>>>> Initial Billboard implementation with 3d objects
+=======
+uniform sampler2D billboardTex;
+>>>>>>> Temporary billboard changes
 
 out vec4 color;
 
